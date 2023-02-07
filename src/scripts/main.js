a20b6eab78bc9d012e2e7375b12e5a2af3c0c36b
Anthony Huang <anthonyhuang07@icloud.com>
2023-02-07 18:40:47 -0500

    shhh

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+const date1 = document.getElementById("date1")
+const date2 = document.getElementById("date2")
+const header = document.getElementById("h-hide")
+let yPos = window.scrollY
+function split(){
+    date1.style.transform = "rotate(-"+(window.scrollY/12.4)+"deg) translate(-"+(window.scrollY/19.1)+"vw)";
+    date2.style.transform = "rotate("+(window.scrollY/12.4)+"deg) translate("+(window.scrollY/19.1)+"vw)";
+    date1.style.opacity = ("1")-(window.scrollY/1000);
+    date2.style.opacity = ("1")-(window.scrollY/1000);  
+}
+
+// hides and shows the header
+function headerHide(){
+    if (yPos < window.scrollY){
+        header.style.top = "-8vh";
+    } else if (yPos > window.scrollY){
+        header.style.top = "0";
+    }
+    yPos = window.scrollY
+}
