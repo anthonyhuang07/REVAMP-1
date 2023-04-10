08640ee2dd46dbf2baca17c9aff93dce34e83a5b
Anthony Huang <anthonyhuang07@icloud.com>
2023-04-10 15:36:30 -0400

    ugategh

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@ const date2 = document.getElementById("date2")
 const header = document.getElementById("h-hide")
 let yPos = window.scrollY
 function split(){
-    date1.style.transform = "rotate(-"+(window.scrollY/12.4)+"deg) translate(-"+(window.scrollY/19.1)+"vw)";
-    date2.style.transform = "rotate("+(window.scrollY/12.4)+"deg) translate("+(window.scrollY/19.1)+"vw)";
+    date1.style.transform = "rotate(-"+(window.scrollY/10)+"deg) translate(-"+(window.scrollY/19.1)+"vw)";
+    date2.style.transform = "rotate("+(window.scrollY/10)+"deg) translate("+(window.scrollY/19.1)+"vw)";
     date1.style.opacity = ("1")-(window.scrollY/1500);
     date2.style.opacity = ("1")-(window.scrollY/1500);  
 }
